6f9f296e1430d09707d3fc3467135a9aa41665fa
cuhpepcekr <lintogm@gmail.com>
2020-06-21 21:46:19 +0900

    Styled-components 버튼 사이즈 조정하기

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ const AppBlock = styled.div`
   padding: 1rem;
 `;
 
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
 const palette = {
   blue: "#228be6",
   gray: "#496057",
@@ -20,9 +25,13 @@ function App() {
   return (
     <ThemeProvider theme={{ palette }}>
       <AppBlock>
-        <Button>BUTTON</Button>
-        <Button color="gray">BUTTON</Button>
-        <Button color="pink">BUTTON</Button>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button color="gray">BUTTON</Button>
+          <Button size="small" color="pink">
+            BUTTON
+          </Button>
+        </ButtonGroup>
       </AppBlock>
     </ThemeProvider>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -18,6 +18,29 @@ const colorStlyes = css`
   }}
 `;
 
+const sizes = {
+  large: {
+    height: "3rem",
+    fontSize: "1.25rem",
+  },
+  medium: {
+    height: "2.25rem",
+    fontSize: "1rem",
+  },
+  small: {
+    height: "1.75rem",
+    fontSize: "0.875rem",
+  },
+};
+
+const sizeStyles = css`
+  /* 크기 */
+  ${({ size }) => css`
+    height: ${sizes[size].height};
+    font-size: ${sizes[size].fontSize};
+  `}
+`;
+
 const StyledButton = styled.button`
   /* 공통 스타일*/
   display: inline-flex;
@@ -31,11 +54,12 @@ const StyledButton = styled.button`
   padding-left: 1rem;
   padding-right: 1rem;
 
-  /* 크기 */
   height: 2.25rem;
   font-size: 1rem;
 
   ${colorStlyes}
+
+  ${sizeStyles}
 
   /* 여백 */
   & + & {
@@ -43,13 +67,13 @@ const StyledButton = styled.button`
   }
 `;
 
-function Button({ children, color, ...rest }) {
+function Button({ children, color, size, ...rest }) {
   return (
-    <StyledButton color={color} {...rest}>
+    <StyledButton color={color} size={size} {...rest}>
       {children}
     </StyledButton>
   );
 }
 
-Button.defaultProps = { color: "blue" };
+Button.defaultProps = { color: "blue", size: "medium" };
 export default Button;
